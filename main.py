d4c0d3ee6d0a4bf7c2c6ed4f7d951c90d234b022
John Bolognino <jcbolo72012@gmail.com>
2020-09-23 12:49:36 -0400

    Added amount recovered and amount isolating stat!

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import requests
 import urllib3
 import os 
 import time
+from datetime import datetime
 import re
 
-def run_webdriver(url): # scrapes url and returns covid data
+def run_webdriver(url, db): # scrapes url and returns covid data
     options = webdriver.ChromeOptions() 
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'chromedriver85.exe')
@@ -20,10 +21,30 @@ def run_webdriver(url): # scrapes url and returns covid data
     time.sleep(10)
     kiddos = driver.find_elements_by_tag_name('rect')
     kiddos = [kid.get_attribute('aria-label') for kid in kiddos]
-    for kid in kiddos:
-        print(kid)
-    return kiddos
 
+    # get the amount of students currently in isolation and total recovered
+
+    current_in_isolation, total_recovered = 0, 0
+
+    cards = driver.find_elements_by_class_name("cardItemContainer")
+    
+    for elem in cards:
+        details = elem.find_element_by_class_name("details")
+
+        # check if we have hit the 'in isolation' statistic
+        if details.get_attribute('title') == "Currently in Isolation":
+            current_in_isolation = elem.find_element_by_class_name("caption").get_attribute("title")
+            now = datetime.now()
+            # send this stat to the database with datestamp
+            add_to_db("Current Isolation", now.strftime("%m/%d/%Y"), current_in_isolation, db)
+
+        # check if we have hit the 'recovered' statistic
+        if details.get_attribute('title') == "Recovered":
+            total_recovered = elem.find_element_by_class_name("caption").get_attribute("title")
+            now = datetime.now()
+            # send this stat to the database with datestamp
+            add_to_db("Recovered", now.strftime("%m/%d/%Y"), total_recovered, db)
+    return kiddos
 
 
 
@@ -42,12 +63,12 @@ def cycle(data, db): # cycles through each new bit of data and sends to db if ne
 
             date = re.compile(r"\d+/\d+/\d\d").search(item)
             tests = re.compile(r"Tests ([\d,]+[.0+]?.)").search(item)
+            
             if date and tests:
-                date = date.group()
-                
-                tests=''.join([x for x in tests.group() if x in "0123456789.,"])
 
-                print(tests)
+                date = date.group()
+                tests=''.join([x for x in tests.group() if x in "0123456789.,"])
+                
                 if data.index(item) < 8:
                     add_to_db("Student Negative", date, tests, db)
                 if data.index(item) > 8 and data.index(item) < 15:
@@ -61,7 +82,9 @@ def cycle(data, db): # cycles through each new bit of data and sends to db if ne
 
 
 if __name__ == "__main__":
-    data = run_webdriver("https://app.powerbi.com/view?r=eyJrIjoiMzI4OTBlMzgtODg5MC00OGEwLThlMDItNGJiNDdjMDU5ODhkIiwidCI6ImQ1N2QzMmNjLWMxMjEtNDg4Zi1iMDdiLWRmZTcwNTY4MGM3MSIsImMiOjN9")
+    print("Connecting to db...")
     db = connect_to_db()
+    print("Scraping most recent statistics...")
+    data = run_webdriver("https://app.powerbi.com/view?r=eyJrIjoiMzI4OTBlMzgtODg5MC00OGEwLThlMDItNGJiNDdjMDU5ODhkIiwidCI6ImQ1N2QzMmNjLWMxMjEtNDg4Zi1iMDdiLWRmZTcwNTY4MGM3MSIsImMiOjN9", db)
     cycle(data, db)
-    print(db.all())
+    print(str(len(db.all())), "objects in database!")
