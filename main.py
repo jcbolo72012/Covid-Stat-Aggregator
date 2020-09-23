26ca78d8ca75c72f5acdc5860356d5f0f5c6b11f
John Bolognino <jcbolo72012@gmail.com>
2020-09-23 01:21:53 -0400

    added db update process, not fully functional yet

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ import requests
 import urllib3
 import os 
 import time
+import re
 
-
-def run_webdriver(url):
+def run_webdriver(url): # scrapes url and returns covid data
     options = webdriver.ChromeOptions() 
     dirname = os.path.dirname(__file__)
     filename = os.path.join(dirname, 'chromedriver85.exe')
     driver = webdriver.Chrome(options=options, executable_path=filename)
     driver.get(url)
-    # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.innerContainer")))
-    # parent = driver.find_element_by_xpath('//*[@id="reportLandingContainer"]/div/exploration-container')
+    
     time.sleep(10)
     kiddos = driver.find_elements_by_tag_name('rect')
     print(kiddos)
@@ -29,29 +28,33 @@ def run_webdriver(url):
 
 
 
-def add_to_db():  # add a column of data to the db
-    return 0
+def add_to_db(type, date, tests, db):  # add a column of data to the db
+    Search = Query()
+    if (db.search(Search.date==date and Search.type == type) == 0):
+        db.insert({'date': date, 'type': type, 'tests': tests})
 
 def connect_to_db(): # initialize connection to tinydb
     db = TinyDB('db.json')
     return db
 
-def scrape(soup):
-    
-    print(soup.a)
-    whole_graph = soup.find("g", id="axisGraphicsContext")
-    print(whole_graph)
-    return 0
+def cycle(data, db):
+    for item in data:
+        date = re.match(r'\d+/\d+/\d\d', item)
+        tests = re.match(r'[\d,]+.00.', item)
+        if data.index(item) < 8 and item != None:
+            add_to_db("Student Negative", date, tests, db)
+        if data.index(item) > 8 and data.index(item) < 15 and item != None:
+            add_to_db("Student Positive", date, tests, db)
+        if data.index(item) > 15 and data.index(item) < 23 and item != None:
+            add_to_db("Faculty Negative", date, tests, db)
+        if data.index(item) > 22 and data.index(item) < 30 and item != None:
+            add_to_db("Faculty Positive", date, tests, db)
+        if data.index(item) > 30 and "New Positive Tests" in item:
+            add_to_db("New Positive Overall", date, tests, db)
 
-def get_the_soup(url):
-    http = urllib3.PoolManager()
-    response = http.request('GET', url)
-    soup = BeautifulSoup(response.data, 'html.parser')
-    print(soup.body)
-
-    return soup
 
 if __name__ == "__main__":
-    run_webdriver("https://app.powerbi.com/view?r=eyJrIjoiMzI4OTBlMzgtODg5MC00OGEwLThlMDItNGJiNDdjMDU5ODhkIiwidCI6ImQ1N2QzMmNjLWMxMjEtNDg4Zi1iMDdiLWRmZTcwNTY4MGM3MSIsImMiOjN9")
-    # soup = get_the_soup("https://app.powerbi.com/view?r=eyJrIjoiMzI4OTBlMzgtODg5MC00OGEwLThlMDItNGJiNDdjMDU5ODhkIiwidCI6ImQ1N2QzMmNjLWMxMjEtNDg4Zi1iMDdiLWRmZTcwNTY4MGM3MSIsImMiOjN9") # https://www.bu.edu/healthway/community-dashboard/
-    # scrape(soup)
+    data = run_webdriver("https://app.powerbi.com/view?r=eyJrIjoiMzI4OTBlMzgtODg5MC00OGEwLThlMDItNGJiNDdjMDU5ODhkIiwidCI6ImQ1N2QzMmNjLWMxMjEtNDg4Zi1iMDdiLWRmZTcwNTY4MGM3MSIsImMiOjN9")
+    db = connect_to_db()
+    cycle(data, db)
+    db.all()
