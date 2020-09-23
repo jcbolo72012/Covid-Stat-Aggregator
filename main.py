b417d64b4f73cab8de2f0bdd5bc1b0aadae55724
John Bolognino <jcbolo72012@gmail.com>
2020-09-23 15:15:14 -0400

    started to integrate seaborn for visualizer

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import os
 import time
 from datetime import datetime
 import re
+import seaborn as sns
+import pandas as pd
+
 
 def run_webdriver(url, db): # scrapes url and returns covid data
     options = webdriver.ChromeOptions() 
@@ -47,7 +50,6 @@ def run_webdriver(url, db): # scrapes url and returns covid data
     return kiddos
 
 
-
 def add_to_db(format, date, tests, db):  # add a column of data to the db
     Search = Query()
     if db.search((Search['date']==date) & (Search['type'] == format)) == []:
@@ -80,11 +82,66 @@ def cycle(data, db): # cycles through each new bit of data and sends to db if ne
                 if data.index(item) > 30 and "New Positive Tests" in item:
                     add_to_db("New Positive Overall", date, tests, db)
 
+def load_from_db(db):
+        Search = Query()
+
+        student_neg = [{x['date']: x['tests']} for x in db.search(Search['type']=="Student Negative")]  
+        student_pos = [{x['date']: x['tests']} for x in db.search(Search['type']=="Student Positive")]  
+        fac_neg = [{x['date']: x['tests']} for x in db.search(Search['type']=="Faculty Negative")]  
+        fac_pos = [{x['date']: x['tests']} for x in db.search(Search['type']=="Faculty Positive")]  
+        new_pos = [{x['date']: x['tests']} for x in db.search(Search['type']=="New Positive Overall")]  
+        dates = [list(x.keys())[0] for x in student_neg]
+        print(dates)
+        stp = []
+        fcp = []
+        for date in dates:
+            try:
+                stp.append(student_pos[date])
+        
+        print(stp)
+        inpv = input("STOP")
+
+
+        data = {'Student Negative': [x[1] for x in student_neg],
+                'Student Positive': [x[1] for x in student_pos],
+                'Faculty Negative': [x[1] for x in fac_neg],
+                'Faculty Positive': [x[1] for x in fac_pos],
+                }
+        print(dates)
+        print(fac_neg)
+        
+        df = pd.DataFrame(data, columns = ['Student Negative', 'Student Positive', 'Faculty Negative', 'Faculty Positive'], index = [date for date in dates])
+        return df
+
+def plot_data(dataset):
+    sns.set_theme()
+    
+    sns.relplot(
+        data=dataset,
+        x=dataset.Date, y=dataset.Tests
+        
+    )
 
 if __name__ == "__main__":
     print("Connecting to db...")
     db = connect_to_db()
-    print("Scraping most recent statistics...")
-    data = run_webdriver("https://app.powerbi.com/view?r=eyJrIjoiMzI4OTBlMzgtODg5MC00OGEwLThlMDItNGJiNDdjMDU5ODhkIiwidCI6ImQ1N2QzMmNjLWMxMjEtNDg4Zi1iMDdiLWRmZTcwNTY4MGM3MSIsImMiOjN9", db)
-    cycle(data, db)
+    # print("Scraping most recent statistics...")
+    # data = run_webdriver("https://app.powerbi.com/view?r=eyJrIjoiMzI4OTBlMzgtODg5MC00OGEwLThlMDItNGJiNDdjMDU5ODhkIiwidCI6ImQ1N2QzMmNjLWMxMjEtNDg4Zi1iMDdiLWRmZTcwNTY4MGM3MSIsImMiOjN9", db)
+    # cycle(data, db)
     print(str(len(db.all())), "objects in database!")
+
+    #  pos_students = load_from_db(db)
+
+    # TODO: Figure out a good way to load & display data
+    print(pos_students)
+
+    plot_data(pos_students)
+    """ 
+    Data Point type labels:
+    
+    Student Negative, 
+    Student Positive, 
+    Faculty Negative, 
+    Faculty Positive, 
+    New Positive Overall
+    """
